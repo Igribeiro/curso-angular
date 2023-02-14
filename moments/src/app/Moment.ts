8e24abc007541d0edf8f47e58be02a4fc53fa7dc
Igor <igoorrib10@gmail.com>
2023-02-14 17:27:44 -0300

    realizando setenças de validações do formulário com reactive form

---
+++ moments/src/app/Moment.ts
@@ -0,0 +1,10 @@
+export interface Moment {
+    id?: number;
+    title: string;
+    description: string;
+    image: string;
+    created_at?: string;
+    updated_at?: string;
+    comments?: [{text: string; username: string}];
+
+}
--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class NewMomentComponent {
   btnText = 'Compartilhar!';
 
+  createHandler(event: any) {
+
+  }
+
 }
